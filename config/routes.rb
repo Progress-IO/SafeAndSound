e40291d349255511673cd981cfbd0ac656c27b85
Daniel <daerodriguezme@unal.edu.co>
2017-09-07 21:06:14 +0000

    Agregada landing page y contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'landing_page/contact'
+  get "contact" => "landing_page#contact", as: "contact"
+  root 'landing_page#index'
 end
