2375f7525c914e9304ce747796a03545e3e63180
Fabian Mendez <ofmendez@unal.edu.co>
2017-09-07 21:17:34 +0000

    *Creado modelo Admi *Configuraciones de Device *Mejorada interfaz de las pantallas de login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  resources :admins
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
   resources :users
+  
+  devise_scope :user do
+    get 'auth/sign_in', to: 'devise/sessions#new'
+  end
+
+  #** Seguro esta linea creará conflicto, borren esta linea para asignar la verdadera landing page 
+  root 'users#index' # REEMPLAZAR POR RUTA DE LANDING PAGE
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
