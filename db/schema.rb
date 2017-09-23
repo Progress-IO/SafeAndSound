dad32414655fdb13956cdc229810bfc736ae7c53
Luis Guzman <leguzmana@unal.edu.co>
2017-09-23 17:41:23 +0000

    forito1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907174814) do
+ActiveRecord::Schema.define(version: 20170923171906) do
 
   create_table "admins", force: :cascade do |t|
     t.string "Name"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20170907174814) do
     t.string "last_sign_in_ip"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "discussions", force: :cascade do |t|
+    t.string "headline"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
