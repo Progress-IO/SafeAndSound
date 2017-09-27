806f310b5a80efb301020c51df98f8f0d5bd92c3
Luis Guzman <leguzmana@unal.edu.co>
2017-09-27 17:13:55 +0000

    mejorado estilo

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20170923183225) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
