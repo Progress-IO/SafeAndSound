e28776b9f09fa42ff7a145898ff0c465e264f8a9
Santiago Peña <spenas@unal.edu.co>
2017-09-28 13:30:52 -0500

    conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170927205211) do
-=======
-ActiveRecord::Schema.define(version: 20170928032607) do
->>>>>>> map-features
+
 
   create_table "admins", force: :cascade do |t|
     t.string "Name"
@@ -34,7 +30,6 @@ ActiveRecord::Schema.define(version: 20170928032607) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-
   create_table "discussions", force: :cascade do |t|
     t.string "headline"
     t.text "content"
@@ -44,7 +39,6 @@ ActiveRecord::Schema.define(version: 20170928032607) do
     t.index ["user_id"], name: "index_discussions_on_user_id"
   end
 
-  
   create_table "reports", force: :cascade do |t|
     t.datetime "fecha"
     t.string "tipo"
