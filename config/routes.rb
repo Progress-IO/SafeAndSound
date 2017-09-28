347c8cf64c156567e0df0cff8b500d4c9ef30ac8
Daniel Esteban Rodríguez Meza <dlsusp@gmail.com>
2017-09-27 19:16:36 -0500

    Agregado partial HEADER y modificado el archivo application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'user_panel/index'
+
   devise_for :admins
   resources :admins
   devise_for :users,:controllers => {:omniauth_callbacks => "users/omniauth_callbacks"},  path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923183225) do
+ActiveRecord::Schema.define(version: 20170923164800) do
 
   create_table "admins", force: :cascade do |t|
     t.string "Name"
@@ -28,15 +28,6 @@ ActiveRecord::Schema.define(version: 20170923183225) do
     t.string "last_sign_in_ip"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
-  create_table "discussions", force: :cascade do |t|
-    t.string "headline"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_discussions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
