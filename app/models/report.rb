bcb059af31d31f14d4b27c271ca2bcbf2affb8ca
Santiago Peña <spenas@unal.edu.co>
2017-10-07 23:23:03 -0500

    Se borro carpeta config2

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,4 @@
 class Report < ApplicationRecord
-    has_attached_file :image, styles:{large:"600x600>" ,medium:"300x300>", thumb:"150x150>"}
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    mount_uploaders :images, ImageUploader
+    serialize :images, JSON # If you use SQLite, add this line.
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004054352) do
+ActiveRecord::Schema.define(version: 20171008032804) do
 
   create_table "admins", force: :cascade do |t|
     t.string "Name"
@@ -48,10 +48,7 @@ ActiveRecord::Schema.define(version: 20171004054352) do
     t.string "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.integer "image_file_size"
-    t.datetime "image_updated_at"
+    t.string "images"
   end
 
   create_table "users", force: :cascade do |t|
