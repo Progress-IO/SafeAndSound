7de59742cade880906e59046e2a5f37d493f5f5f
Santiago Peña <spenas@unal.edu.co>
2017-10-11 11:47:19 -0500

    añadida linea config.assets.precompile = ['*.js', '*.css', '*.css.erb','*.png','*.jpg'] en production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
- 
+  config.assets.precompile = ['*.png','*.jpg']
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
