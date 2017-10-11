cf588c186da8f751616a9626247c6140543382b9
Fabian Mendez <ofmendez@gmail.com>
2017-10-11 15:16:52 -0500

    Organizacion del archivo de rutas y evitando acceso a rutas que requieren login

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,27 @@
 Rails.application.routes.draw do
-    resources :routes
-    resources :suspects
-    resources :reports
+    # devise_for :admins
+    # resources :admins
+    # resources :users
+    
     # get 'user_panel/index'
-    get '/users_panel' =>'user_panel#index', :as => :user_panel
-    get '/users_panel/report' =>'user_panel#report', :as => :report_type
-
-    resources :discussions
-    devise_for :admins
-    resources :admins
     devise_for :users,:controllers => {:omniauth_callbacks => "users/omniauth_callbacks"},  path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' }
+    get '/users_panel/report' =>'user_panel#report', :as => :report_type
+    get '/users_panel' =>'user_panel#index', :as => :user_panel
+    get "landing_page/contact", as: "contact"
+    root 'landing_page#index'
+    
+    authenticate :user do
+        resources :routes
+        resources :suspects
+        resources :reports
+        resources :discussions
+        get 'reports/index'
+        get "/user_panel" => 'user_panel#index', as: :user_root
+    end
 
-    resources :users
+    namespace :user do
+        root "user_panel#index"
+    end
 
     devise_scope :user do
         get 'auth/sign_in', to: 'devise/sessions#new'
@@ -19,20 +29,7 @@ Rails.application.routes.draw do
         get 'auth/sign_out', to: 'devise/sessions#destroy'
     end
 
-
-    get "landing_page/contact", as: "contact"
-
     get "auth/login" => "users/sessions#new", as: "login"
     get "auth/registration" => "user/registrations#new", as: "register"
-    get 'reports/index'
 
-
-    # User_panel redirect
-    get "/user_panel" => 'user_panel#index', as: :user_root
-
-    namespace :user do
-        root "user_panel#index"
-    end
-
-    root 'landing_page#index'
 end
