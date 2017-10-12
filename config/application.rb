84faff36ec4e223548c5faa2f0445b2e02513f8c
Juan David <judvalenciaan@unal.edu.co>
2017-10-12 20:52:07 +0000

    Bug fix: config/application assets

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,6 @@ module SafeAndSound
     config.load_defaults 5.1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
 
     # Removidos tags agregados por Devise al existir un error.
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag.html_safe }
