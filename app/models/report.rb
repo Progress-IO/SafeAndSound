febbd907e7f7a4a0d57f2cd442e623f0e4feb6bc
Santiago Peña <spenas@unal.edu.co>
2017-10-20 14:58:53 -0500

    Corregido error con las fechas

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,10 @@
 class Report < ApplicationRecord
+    # attr_accessible :dia, :hora, :fecha
     mount_uploaders :images, ImageUploader
     serialize :images, JSON # If you use SQLite, add this line.
+
+   
+
+    
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012021047) do
+ActiveRecord::Schema.define(version: 20171020172742) do
 
   create_table "admins", force: :cascade do |t|
     t.string "Name"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20171012021047) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "images"
+    t.date "dia"
+    t.time "hora"
   end
 
   create_table "routes", force: :cascade do |t|
@@ -75,6 +77,8 @@ ActiveRecord::Schema.define(version: 20171012021047) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "images"
+    t.date "dia"
+    t.time "hora"
   end
 
   create_table "users", force: :cascade do |t|
