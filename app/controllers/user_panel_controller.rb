49dbcbe373e98ddc7d71c9a004edb0084fb9fe71
Juan David Valencia <judvalenciaan@unal.edu.co>
2017-10-21 10:47:25 -0500

    Adding chart to pdf

--- app/controllers/user_panel_controller.rb
+++ app/controllers/user_panel_controller.rb
@@ -20,7 +20,7 @@ class UserPanelController < ApplicationController
         format.pdf do
           render template: "user_panel/pdf.html.erb",
           pdf:"pdf",
-          javascript_delay: 5000
+          javascript_delay: 1000
         end
       end
   end
