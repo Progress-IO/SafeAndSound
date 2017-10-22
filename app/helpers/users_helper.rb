e8fb7389df334664fb4f4abedb74e205f91f757e
Luis Guzman <leguzmana@unal.edu.co>
2017-10-22 03:57:36 +0000

    mejorado avatar y seccion de editar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+    def avatar_for(user, options= {size:50})
+        size= options[:size]
+        if user.avatar?
+            image_tag user.avatar.url(:small), width: size, class:"avatar-image"
+            
+        else
+            image_tag "avatar.png", width: size, class:"avatar-image"
+            
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022014523) do
+ActiveRecord::Schema.define(version: 20171022032034) do
 
   create_table "admins", force: :cascade do |t|
     t.string "Name"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20171022014523) do
     t.string "uid"
     t.string "username"
     t.string "avatar"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
