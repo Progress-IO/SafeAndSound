08a889e6c8f80ad188f095d6d39c6d6dc1309fc7
Luis Guzman <leguzmana@unal.edu.co>
2017-10-24 20:30:11 +0000

    arreglado link ajustes

--- config/routes.rb
+++ config/routes.rb
@@ -8,27 +8,33 @@ Rails.application.routes.draw do
     get '/users_panel/report' =>'user_panel#report', :as => :report_type
     get "landing_page/contact", as: "contact"
     get 'reports' => 'reports#index', :as => :reports_list;
-   
+    
     root 'landing_page#index'
-     resources :users, except:[:destroy]
+    
+    
+     resources :users, except:[:destroy] 
+    
     authenticate :user do
         resources :routes
         resources :suspects
         resources :reports
         resources :discussions
-        
+       
         
        # get '/users/:id'  => 'users#show', :as => :user
        
        # get '/users/:id/edit' => 'users#edit' , :as => :edit_user
     #patch '/users/:id/edit' => 'users#update' 
     #    put '/users/:id/update' => 'users#update'
-         
+        #get '/users/:id'  => 'users#show', :as => :profile
+       
         get '/users_panel' =>'user_panel#index', :as => :user_panel
         get 'reports/index'
         get "/user_panel" => 'user_panel#index', as: :user_root
         get '/users_panel/statistics' =>'user_panel#statistics', :as => :statistics
     end
+
+    
 
     namespace :user do
         root "user_panel#index"
@@ -39,7 +45,7 @@ Rails.application.routes.draw do
     devise_scope :user do
         get 'auth/sign_in', to: 'devise/sessions#new'
         get 'auth/registration', to: 'devise/registrations#new'
-       
+        
         delete 'auth/sign_out', to: 'devise/sessions#destroy'
     end
 
