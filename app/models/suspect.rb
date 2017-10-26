d12ee653b23671cc308fa5970e340e8ad8fb0c8d
Juan David Valencia <judvalenciaan@unal.edu.co>
2017-10-26 06:41:28 -0500

    Adding: 'Validations for suspects'

--- app/models/suspect.rb
+++ app/models/suspect.rb
@@ -2,4 +2,7 @@ class Suspect < ApplicationRecord
     mount_uploaders :images, ImageUploader
     serialize :images, JSON # If you use SQLite, add this line.
     belongs_to :user
+    # Validation of marker
+    validates :latitude, presence: true
+    validates :longitude, presence: { message: "Marker must be added" }
 end
