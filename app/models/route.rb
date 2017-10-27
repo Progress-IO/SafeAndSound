0fd85a640537efea11d0a539ac2a19bc442399b2
Juan David Valencia <judvalenciaan@unal.edu.co>
2017-10-27 09:02:51 -0500

    Adding: 'Validation for routes'

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,5 +1,9 @@
 class Route < ApplicationRecord
     belongs_to :user
+    validates :origin_latitude, presence: true
+    validates :destination_latitude, presence: true
+    validates :destination_longitude, presence: true
+    validates :origin_longitude, presence: { message: "Marker must be added" }
 
     def self.Reports_position
         info = []
