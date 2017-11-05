32e71af2953c64a0d3ead098a8e0b39d8831fd2f
Luis Guzman <leguzmana@unal.edu.co>
2017-11-05 13:41:53 +0000

    mejorada paginacion..

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,11 +1,11 @@
 $(function() {
-  if ($('#infinite-scrolling').size() > 0) {
-    return $('#listapanel').on('scroll', function() {
+  if ($('#infinite-scrolling-suspects').size() > 0) {
+    return $('.suspect_list').on('scroll', function() {
       var more_posts_url;
-      more_posts_url = $('.pagination .next_page').attr('href');
-      if (more_posts_url && $('#listapanel').scrollTop() > $('#suspects').height() - 350) {
+      more_posts_url = $('.paginationsu .next_page').attr('href');
+      if (more_posts_url && $('.suspect_list').scrollTop() > $('#suspects').height() - 450) {
        // alert($('#test1 .suspect_list').scrollTop() + " px is more than "+  $('#suspects').height() +"- 350" );
-        $('.pagination').html('<div><img src="assets/loading.gif" alt="Loading..." title="Loading..."  </a></div>');
+        $('.paginationsu').html('<div><img src="assets/loading.gif" alt="Loading..." title="Loading..."  </a></div>');
         $.getScript(more_posts_url);
       }
       return;
@@ -15,12 +15,12 @@ $(function() {
 
 $(function() {
   if ($('#infinite-scrolling-reports').size() > 0) {
-    return $('#test2 .suspect_list').on('scroll', function() {
+    return $('.report_list').on('scroll', function() {
       var more_posts_url;
-      more_posts_url = $('.pagination .next_page').attr('href');
-      if (more_posts_url && $('#test2 .suspect_list').scrollTop() > $('#reports').height() - 450) {
+      more_posts_url = $('.paginationre .next_page').attr('href');
+      if (more_posts_url && $('.report_list').scrollTop() > $('#reports').height() - 450) {
        // alert($('#test1 .suspect_list').scrollTop() + " px is more than "+  $('#suspects').height() +"- 350" );
-        $('.pagination').html('<div><img src="assets/loading.gif" alt="Loading..." title="Loading..."  </a></div>');
+        $('.paginationre').html('<div><img src="assets/loading.gif" alt="Loading..." title="Loading..."  </a></div>');
         $.getScript(more_posts_url);
       }
       return;
@@ -30,12 +30,12 @@ $(function() {
 
 $(function() {
   if ($('#infinite-scrolling-transports').size() > 0) {
-    return $('#test3 .suspect_list').on('scroll', function() {
+    return $('.transport_list').on('scroll', function() {
       var more_posts_url;
-      more_posts_url = $('.pagination .next_page').attr('href');
-      if (more_posts_url && $('#test3 .suspect_list').scrollTop() > $('#transports').height() - 350) {
+      more_posts_url = $('.paginationtr .next_page').attr('href');
+      if (more_posts_url && $('.transport_list').scrollTop() > $('#transports').height() - 450) {
        // alert($('#test1 .suspect_list').scrollTop() + " px is more than "+  $('#suspects').height() +"- 350" );
-        $('.pagination').html('<div><img src="assets/loading.gif" alt="Loading..." title="Loading..."  </a></div>');
+        $('.paginationtr').html('<div><img src="assets/loading.gif" alt="Loading..." title="Loading..."  </a></div>');
         $.getScript(more_posts_url);
       }
       return;
